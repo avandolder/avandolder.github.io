b470d8a7a656f8cc0547364eeedeb133bc2864fa
Adam Vandolder <adam.vandolder@gmail.com>
2019-07-25 13:29:30 -0400

    Simplified script

--- script.js
+++ script.js
@@ -3,9 +3,8 @@
 (function(){
     const footerElement = document.getElementById("main-footer");
     const spacer = document.createElement("br");
-    const lastModified = new Date(document.lastModified);
     const lastModifiedText =
-        document.createTextNode("Last Modified: " + lastModified.toString());
+        document.createTextNode("Last Modified: " + document.lastModified);
 
     footerElement.appendChild(spacer);
     footerElement.appendChild(lastModifiedText);
