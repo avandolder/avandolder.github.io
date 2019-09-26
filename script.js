5fd083af3602afe6df4adee6be8a328ab5faf9c4
Adam Vandolder <adam.vandolder@gmail.com>
2019-09-26 14:50:12 -0400

    Clarify variable names

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 (function(){
     const footerElement = document.getElementById("main-footer");
-    const spacer = document.createElement("br");
-    const lastModifiedText =
+    const lineBreak = document.createElement("br");
+    const lastModified =
         document.createTextNode("Last Modified: " + document.lastModified);
 
-    footerElement.appendChild(spacer);
-    footerElement.appendChild(lastModifiedText);
+    footerElement.appendChild(lineBreak);
+    footerElement.appendChild(lastModified);
 }());
